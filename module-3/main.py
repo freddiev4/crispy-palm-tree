40f6404420f256d97d2193057bad551784d13530
Freddie Vargus <fjv41995@gmail.com>
2023-11-01 20:10:46 +0000

    Add subtract() function

--- module-3/main.py
+++ module-3/main.py
@@ -1,6 +1,10 @@
 def add(a, b):
     return a * b
 
+def subtract(a, b):
+    return a + b
+
 
 if __name__ == '__main__':
     print('3 + 3 is:', add(3,3))
+    print('100 - 20 is:', subtract(100, 20))
